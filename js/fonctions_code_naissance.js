0011eaecf942fe57b6cdbe58992894d40bcf5d47
chohans <mihael@aliceadsl.fr>
2015-04-05 14:17:05 +0200

    ajoue du main.js, du fonctions_code_naissance.js et  de l'image select2.png

---
+++ js/fonctions_code_naissance.js
@@ -0,0 +1,117 @@
+
+function calculYear(date)
+{
+    var newYear = "", // variablme contenant le calcul de l'année
+        an ="", // variable contenat l'année reçue de la date
+        mois ="", // variable contenant le mois reçue de la date
+        jour ="", // variable contenat le jour reçue de la date
+        l = date.length, // longueur de la date avec les "/"
+        tabCibles = []; //on y met l'an cible et le jour cible'
+
+    an = date.substr(l-4, 4); // récuperation des 4 chiffres de l'année reçue
+
+    date = date.slice(0,l-5); // on prend le debut de la date jusqu'a la fin du mois
+
+
+    for(var l=date.length-1; l>=0; l--)
+    {
+        /*  Je cherche le passage entre le jour et le mois en cherchant le "/"
+         ** puis extraction de la partie contenant le mois
+        */
+        if(date[l]== "/")
+        {
+            mois = date.substr(l+1); // récuperation du mois
+            jour = date.slice(0, l); // retire le mois reste le jour mit en mémoire
+        }
+    }
+
+    newYear = parseInt(an) + parseInt(mois) + parseInt(jour);
+    tabCibles.push(newYear);
+    tabCibles.push(jour + "/" + mois);
+
+    return tabCibles;
+}
+
+
+function calculCle (date)
+{
+    var newDate = extraire(date);
+    var chemin = add(newDate);
+    var resource = addPlus(chemin);
+
+    resource.unshift(chemin);
+
+    return resource;
+}
+
+
+function addPlus(nombre)
+{
+    nombre = nombre.toString();
+    var total = 0, sousTotal = nombre,
+        l = nombre.length;
+    var tabTotaux = [];
+
+    if(l>1){
+        for(var i =0; i<l; i++)
+        {
+            total += parseInt(nombre[i]);
+        }
+    }
+    else{
+        total = parseInt(nombre);
+    }
+
+    if(total>9)
+    {
+        sousTotal = total;
+        total = (total%10)+((total-(total%10))/10);
+        tabTotaux.push(total, sousTotal);
+    }
+    else
+    {
+        if(total != nombre){
+            tabTotaux.push(total);
+        }
+        else{
+            tabTotaux = [];
+        }
+    }
+
+    return tabTotaux;
+}
+
+
+function extraire(date)
+{
+    //var str = nombre.toString();
+    var l= date.length;
+    var tabChiffre = [];
+
+    for(var i =0; i<l; i++)
+    {
+        if(date[i]!= "/"){
+            tabChiffre.push(date[i]);
+        }
+    }
+
+    return tabChiffre;
+}
+
+
+function add(nombre)
+{
+    var total = 0,
+        l = nombre.length;
+
+    if(l>1){
+        for(var i = 0; i<=l-1; i++){
+            total += parseInt(nombre[i]);
+        }
+    }
+    else{
+        total = parseInt(nombre);
+    }
+
+    return total;
+}
---
+++ js/main.js
@@ -0,0 +1,149 @@
+/*
+ * Main début du programme
+ */
+
+$(function(){
+
+/*
+ * Initialise le selecteur et charge les donnée du local storage
+ */
+    var $patients = $("#patients");
+    $patients.select2({
+        placeholder: "Choissir un patient",
+        allowClear: true,
+        data: _.map($.jStorage.index(), function(nomPatient){
+            return {
+                id: nomPatient,
+                text: nomPatient
+            }
+        })
+    });
+
+    $("#newPatient").click(function(){
+        $("#newPatientContainer").toggle("slow");
+    });
+
+    /* Mets en mémoire dans la var patient
+     * les données introduits
+     * dans le formulaire
+     */
+
+    var $name = $("#name");
+    var $birthdate = $("#birthdate");
+
+    $("#createNewPatient").click(function(){
+        var patient = {
+            name: $name.val(),
+            birthdate: $birthdate.val()
+        };
+
+        $.jStorage.set(patient.name, patient) // Mets les données dans le local storage
+
+        // efface le contenu de l'input nom et date'
+        $name.val(null);
+        $birthdate.val(null);
+
+        $("#newPatientContainer").hide("slow"); // referme le formulaire automatiquement
+
+        /*
+        * réactualise la liste des noms des patients
+        */
+
+        $patients.select2({
+            placeholder: "Choissir un patient",
+            allowClear: true,
+            data: _.map($.jStorage.index(), function(nomPatient){
+                return {
+                    id: nomPatient,
+                    text: nomPatient
+                }
+            })
+        });
+    });
+
+    /*
+     *  En cliquant sur le bouton 'annuler' le formulaire se referme
+     */
+
+    $("#cancelNewPatient").click(function(){
+        $("#newPatientContainer").hide("slow");
+    } );
+
+    /*
+ *  selection d'un autre nom dans la liste disponible
+ */
+    $patients.on("change", function(selection){
+        var patient = $.jStorage.get(selection.val);
+
+        changePatient(patient); // envoie des données du patients
+    });
+
+    //        Object {name: "tonio", birthdate: "1963-05-17"}
+/*
+ *  Calcul des 3 autrees dates de la personne choisie et des 4 clés
+ *  des 4 jours cibles et des 4 ans cibles
+ */
+    var changePatient = function(patient){
+        var naissance = formatDate(moment(patient.birthdate));
+        var keyN = formatKey(calculCle(naissance));
+        var anCibleN = calculYear(naissance);
+        var conception = formatDate(moment(patient.birthdate).subtract(9, "M"));
+        var KeyC = formatKey(calculCle(conception));
+        var anCibleC = calculYear(conception);
+        var m9 = formatDate(moment(patient.birthdate).subtract(18, "M"));
+        var KeyM9 = formatKey(calculCle(m9));
+        var anCibleM9 = calculYear(m9);
+        var p9 = formatDate(moment(patient.birthdate).add(9, "M"));
+        var KeyP9 = formatKey(calculCle(p9));
+        var anCibleP9 = calculYear(p9);
+
+    /*
+     *  Ecriture des 4 date fondatrices
+     */
+        $("#nDate").text(naissance);
+        $("#cDate").text(conception);
+        $("#m9Date").text(m9);
+        $("#p9Date").text(p9);
+     /*
+     *  Ecriture des 4 clés
+     */
+        $("#keyN").text(keyN);
+        $("#keyC").text(KeyC);
+        $("#key-9").text(KeyM9);
+        $("#keyP9").text(KeyP9);
+    /*
+     *  Ecriture des 4 jours cibles
+     */
+        $("#jourCibleN").text(anCibleN[1]);
+        $("#jourCibleC").text(anCibleC[1]);
+        $("#jourCible-9").text(anCibleM9[1]);
+        $("#jourCibleP9").text(anCibleP9[1]);
+    /*
+     *  Ecriture des 4 ans cibles
+     */
+        $("#anCibleN").text(anCibleN[0]);
+        $("#anCibleC").text(anCibleC[0]);
+        $("#anCible-9").text(anCibleM9[0]);
+        $("#anCibleP9").text(anCibleP9[0]);
+
+    }
+
+    /*
+     *  formatage final de la date
+     */
+    var formatDate = function(date)
+    {
+        return date.format("D/M/YYYY");
+    }
+    /*
+     *  formatage final de la clé de naissance
+     */
+    var formatKey = function(cle)
+    {
+
+        return cle.join("/");
+    }
+});
+
+
+
