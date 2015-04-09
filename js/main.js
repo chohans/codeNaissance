4f8f76cef9d74d4a75578dd870feb1141ca75a34
chohans <mihael@aliceadsl.fr>
2015-04-09 11:01:23 +0200

    correction pour les essences à 3 chiffres

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@ $(function(){
         $("#cleNaissance").text(keyN[1]);
         $("#clePlus9").text(KeyP9[1]);
 
-        var totalEssence = sup9(KeyM9[1] + KeyC[1] + keyN[1] + KeyP9[1]),
+        var totalEssence = KeyM9[1] + KeyC[1] + keyN[1] + KeyP9[1],
             totalTriangleP = parseInt(sup9(KeyM9[1] + KeyC[1])),
             totalTriangleR = parseInt(sup9(KeyC[1] + keyN[1])),
             totalTriangleC = parseInt(sup9(keyN[1] + KeyP9[1])),
@@ -157,11 +157,11 @@ $(function(){
             totalSommeInterne = sup9(KeyC[1] + keyN[1]),
             totalValeurSupreme =sup9(KeyM9[1] + KeyP9[1]);
 
-        $("#essence").text(totalEssence[1]+"/"+totalEssence[0]);
+        $("#essence").text(totalEssence+"/"+ (sup9(totalEssence))[0]);
         $("#triangleP").text(totalTriangleP);
         $("#triangleR").text(totalTriangleR);
         $("#triangleC").text(totalTriangleC);
-        $("#transformation").text(totalTransformation + "/" + addPlus(totalTransformation));
+        $("#transformation").text(totalTransformation + "/" + sup9(totalTransformation));
         $("#accordIncarnation1").text(totalIncarnation1);
         $("#accordIncarnation2").text(totalIncarnation2);
         $("#incarnation").text(totalIncarnation[0]);
